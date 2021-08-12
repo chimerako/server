b596cc129430dee69b390bd632cd5b865768c5a3
chimerako <65267814+chimerako@users.noreply.github.com>
2021-08-12 11:31:10 +0700

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,3 +1,5 @@
 function collectiveray_theme_scripts_function() {
   wp_enqueue_script( 'js-file', get_template_directory_uri() . '/js/view.js');
 }
+
+add_action('wp_enqueue_scripts','collectiveray_theme_scripts_function');
